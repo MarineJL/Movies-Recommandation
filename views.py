c1dd0c9f5e1acafd1f1ad36589925249ad2c741f
MarineJL <marine.jacquemin-lorriaux@edhec.com>
2018-10-29 16:30:25 +0100

    init commit

---
+++ views.py
@@ -0,0 +1,16 @@
+from flask import Flask
+import pandas as pd
+
+app = Flask(__name__)
+
+@app.route('/recommend/<int:film_id>')
+def show_post(film_id):
+	response = 'Film not found'
+	knn_tbl = pd.read_csv("/Users/marinejacquemin/Desktop/knn_tbl.csv")
+	print('Film id passe est : ' + str(film_id))
+	response = "You've picked " + knn_tbl.loc[film_id,'Film'] + " I recommend you : " + str(knn_tbl.loc[film_id,['Film1','Film2','Film3','Film4','Film5']])
+	return response
+
+
+if __name__ == "__main__":
+    app.run()
